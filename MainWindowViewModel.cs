7e51d9fa0fc9802cbf7787326d2732989db2ae1f
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2013-11-21 17:50:08 +0900

    initial commit

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+  public class MainWindowViewModel
+  {
+    public string Greeging
+    {
+      get
+      {
+        return "Hello!";
+      }
+    }
+    public event EventHandler Hide;
+
+    public void HideWindow()
+    {
+      var handler = Hide;
+      if (handler != null)
+      {
+        handler(this, EventArgs.Empty);
+      }
+    }
+  }
+}
